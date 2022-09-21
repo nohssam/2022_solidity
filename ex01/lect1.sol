bd5d47a806bb5e6a502b5c51cec8649660c92976
nohssam <78917104+nohssam@users.noreply.github.com>
2022-09-21 21:32:27 +0900

    Create lect1.sol

---
+++ ex01/lect1.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0 : 이 라이센스는 무조건 맨위에 명시 해주셔야 에러가 나지 않습니다.
+// pragma solidity >=0.7.0 <0.9.0; : 솔리디티의 컴파일 버전 명시 입니다 ( 즉 0.7 ~0.9 의 버전을 사용)
+//  스마트컨트랙이란 
+// contract Hello{ : 스마트 컨트랙 명시 입니다.  
+//  string public hi = "Hello solidity";  : hi 라는 public 함수에 hello solidity 를 넣었습니다. 
+// }
+
+// 라이센스 명시
+pragma solidity >= 0.7.0 < 0.9.0 ;
+
+contract Hello{
+    string public hi = "Hello solidity";
+}
